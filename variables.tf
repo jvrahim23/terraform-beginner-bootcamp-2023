9b8108584d96fdfea68582c64dee3f0a715854f4
jwilson23 <jvrahim23@yahoo.com>
2023-10-02 21:02:57 +0000

    #24 bucket no longer uses random provider

--- variables.tf
+++ variables.tf
@@ -1,8 +1,21 @@
 variable "user_uuid" {
-  type    = string
-  description = "User UUID in UUID format (e.g., 123e4567-e89b-12d3-a456-426614174000)"
+  description = "The UUID of the user"
+  type        = string
   validation {
-    condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$", var.user_uuid))
-     error_message = "Invalid user_uuid format. It should be in UUID format (e.g., 123e4567-e89b-12d3-a456-426614174000)."
+    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message    = "The user_uuid value is not a valid UUID."
+  }
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+
+  validation {
+    condition     = (
+      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
+      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
+    )
+    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
   }
 }
