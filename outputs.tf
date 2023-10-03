b135f11db373e1b28ca3405df05e7e2e05c6092a
jwilson23 <63145873+jvrahim23@users.noreply.github.com>
2023-10-03 11:59:16 +0200

    #28 enabled static website hosting, upload ind/er (#29)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
 }
+
+
+output "s3_website_endpoint" {
+  description = "S3 Statoc Website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
