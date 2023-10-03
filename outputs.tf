e34f6e4647ba554a474078ad8f611353f3569570
jwilson23 <63145873+jvrahim23@users.noreply.github.com>
2023-10-03 18:31:13 +0200

    #30 created cdn and serve s3 bucket from cdn (#31)

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,7 @@ output "bucket_name" {
   value = module.terrahouse_aws.bucket_name
 }
 
-
-output "s3_website_endpoint" {
-  description = "S3 Statoc Website hosting endpoint"
+output "website_endpoint" {
+  description = "S3 Static Website hosting endpoint"
   value = module.terrahouse_aws.website_endpoint
 }
